25f651e82e3285ffa8f33b28c5c5920c20a300d0
patelshivani3 <shivpatel3001@gmail.com>
2022-12-23 22:22:29 +0530

    Adding FlipCoin program

---
+++ BasicCoreProgramsDay5/FlipCoin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay5
+{
+    internal class FlipCoin
+    {
+        public static void CoinPercentage()
+        {
+            int tailCount = 0, headCount = 0;
+            Console.WriteLine("Enter the number to flip the coin : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+
+            for (int i = 1; i < n; i++)
+            {
+                double input = random.NextDouble();
+                if (input < 0.5)
+                    tailCount++;
+                else
+                    headCount++;
+            }
+            Console.WriteLine("TailCount:{0} & HeadCount:{1}", tailCount, headCount);
+            double tailPercentage = (tailCount * 100) / n;
+            double headPercentage = (headCount * 100) / n;
+            Console.WriteLine("TailCount:{0}% & HeadCount:{1}%", tailPercentage, headPercentage);
+        }
+    }
+}
+
+
---
+++ BasicCoreProgramsDay5/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to basic core programs");
+            Console.WriteLine("1.FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    FlipCoin.CoinPercentage();
+                    break;
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
