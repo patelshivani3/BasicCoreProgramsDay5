c6a612f85418e04366a2f7dfba2ecbcab77e2bba
patelshivani3 <shivpatel3001@gmail.com>
2022-12-25 10:28:23 -0800

    Adding EvenOdd Program

---
+++ BasicCoreProgramsDay5/EvenOdd.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay5
+{
+    internal class EvenOdd
+    {
+        public static void Number()
+        {
+            Console.Write("\nEnter the number to check : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n % 2 == 0)
+                Console.WriteLine("\n{0} is Even Number", n);
+            else
+                Console.WriteLine("\n{0} is Odd Number", n);
+        }
+    }
+}
