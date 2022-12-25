7792c6853dba56dcde63fa52e024084a6e899709
patelshivani3 <shivpatel3001@gmail.com>
2022-12-25 09:34:30 -0800

    Adding Prime Factor Program

---
+++ BasicCoreProgramsDay5/PrimeFactors.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgramsDay5
+{
+    internal class PrimeFactors
+    {
+        public static void Factors()
+        { 
+            Console.WriteLine("\nEnter the number to find prime factors : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i = 2; i<=n; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i+ " ");
+                    n=n/i;
+                }
+            }
+
+        }
+    }
+}
--- BasicCoreProgramsDay5/Program.cs
+++ BasicCoreProgramsDay5/Program.cs
@@ -11,7 +11,7 @@ namespace BasicCoreProgramsDay5
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to basic core programs");
-            Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.Power Of 2\n4.Harmonic Number");
+            Console.WriteLine("1.FlipCoin\n2.LeapYear\n3.Power Of 2\n4.Harmonic Number\n5.Prime Factors");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
@@ -26,8 +26,13 @@ namespace BasicCoreProgramsDay5
                 case 3:
                     PowerOf2.powerOf2();
                     break;
+               
                 case 4:
                     HarmonicNumber.harmonicNumber(); 
+                    break;
+                
+                case 5:
+                    PrimeFactors.Factors(); 
                     break;
             }
             Console.ReadLine();
